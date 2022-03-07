79fb98f162f262d81e1d2eda8a7047e8fd067c96
Marcin Pajkowski <mpa@genesismobo.com>
2022-03-07 17:31:47 +0100

    uplift to newest pgx

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
-use std::cmp::Ordering;
-
 use pgx::*;
 
+use std::cmp::Ordering;
+
 pg_module_magic!();
+
+extension_sql_file!(
+    "../sql/operators.sql",
+    finalize //requires = [natord_gt, natord_lt, natord_ge, natord_le, natord_cmp]
+);
 
 #[pg_extern]
 fn natord_gt(lhs: &str, rhs: &str) -> bool {
@@ -23,6 +28,11 @@ fn natord_ge(lhs: &str, rhs: &str) -> bool {
 }
 
 #[pg_extern]
+fn natord_le(lhs: &str, rhs: &str) -> bool {
+    matches!(natord::compare(lhs, rhs), Ordering::Less | Ordering::Equal)
+}
+
+#[pg_extern]
 fn natord_cmp(lhs: &str, rhs: &str) -> i32 {
     match natord::compare(lhs, rhs) {
         Ordering::Less => -1,
@@ -31,12 +41,8 @@ fn natord_cmp(lhs: &str, rhs: &str) -> i32 {
     }
 }
 
-#[pg_extern]
-fn natord_le(lhs: &str, rhs: &str) -> bool {
-    matches!(natord::compare(lhs, rhs), Ordering::Less | Ordering::Equal)
-}
-
 #[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
 mod tests {
     use super::*;
 
@@ -45,5 +51,17 @@ mod tests {
         assert_eq!(1, natord_cmp("112A", "112"));
         assert_eq!(0, natord_cmp("112A", "112A"));
         assert_eq!(-1, natord_cmp("112A", "112B"));
+    }
+}
+
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
     }
 }
